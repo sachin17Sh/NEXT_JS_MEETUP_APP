270d77d1f73ba4bcd7727947c4f6bd31fc903a4c
Sachin Sharma <ss0382245@gmail.com>
2025-05-19 16:33:18 +0530

    adding the data fatching logic

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,12 +1,33 @@
+import { useRouter } from "next/router"
 import NewMeetupForm from "../../components/meetups/NewMeetupForm"
+import Head from "next/head"
 
 export default function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData){
-      console.log(enteredMeetupData)
+  const router = useRouter()
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch('api/new-meetup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(enteredMeetupData)
+    })
+    const data = await response.json()
+    console.log(data)
+    if (data) {
+      router.push('/')
+    }
+    else {
+      console.log("An error occurred")
+    }
   }
   return (
     <>
-    <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta name="description" content="Add a new meetup arcoss any location" />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </>
   )
 }
